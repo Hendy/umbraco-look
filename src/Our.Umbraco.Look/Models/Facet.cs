81032763df24210a1fce6156a0edcbbbfb46fde4
Hendy <hendyracher@hotmail.com>
2018-10-25 21:08:49 +0100

    fleshed out Facet poco (not using a Tag poco to keep API simple)

--- src/Our.Umbraco.Look/Models/Facet.cs
+++ src/Our.Umbraco.Look/Models/Facet.cs
@@ -1,13 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Our.Umbraco.Look.Models
+﻿namespace Our.Umbraco.Look.Models
 {
     public class Facet
     {
+        /// <summary>
+        /// The name of the tag
+        /// </summary>
+        public string Tag { get; internal set; }
 
+        /// <summary>
+        /// The total number of results expected should this tag be added to TagQuery.AllTags on the current query
+        /// </summary>
+        public int Count { get; internal set; }
     }
 }
