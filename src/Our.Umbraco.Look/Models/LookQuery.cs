b8344a4008c0519ba7651d7eeda9035c4015d81f
Hendy <hendyracher@hotmail.com>
2018-11-02 23:14:23 +0000

    added RawQuery property & constructor overload to LookQuery & reordered properties

--- src/Our.Umbraco.Look/Models/LookQuery.cs
+++ src/Our.Umbraco.Look/Models/LookQuery.cs
@@ -6,6 +6,26 @@
     public class LookQuery
     {
         /// <summary>
+        /// A raw Lucene query string
+        /// </summary>
+        public string RawQuery { get; set; }
+
+        /// <summary>
+        /// Specify (optional) docType aliases to include, or node ids to exclude
+        /// </summary>
+        public NodeQuery NodeQuery { get; set; } = new NodeQuery();
+
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public NameQuery NameQuery { get; set; } = new NameQuery();
+
+        /// <summary>
+        /// Set a before and/or an after date
+        /// </summary>
+        public DateQuery DateQuery { get; set; } = new DateQuery();
+
+        /// <summary>
         /// Search text, and configuration options for highlighting (if required)
         /// </summary>
         public TextQuery TextQuery { get; set; } = new TextQuery();
@@ -14,21 +34,6 @@
         /// Search tags, configuring collections that are 'required', and/or 'grouped ors'
         /// </summary>
         public TagQuery TagQuery { get; set; } = new TagQuery();
-
-        /// <summary>
-        /// Set a before and/or an after date
-        /// </summary>
-        public DateQuery DateQuery { get; set; } = new DateQuery();
-
-        ///// <summary>
-        ///// 
-        ///// </summary>
-        //public NameQuery NameQuery { get; set; } = new NameQuery();
-
-        /// <summary>
-        /// Specify (optional) docType aliases to include, or node ids to exclude
-        /// </summary>
-        public NodeQuery NodeQuery { get; set; } = new NodeQuery();
 
         /// <summary>
         /// specifify criteria to perform a location distance query
@@ -47,8 +52,13 @@
         {
         }
 
-        //public SearchQuery(string rawQuery)
-        //{
-        //}
+        /// <summary>
+        /// Constructor - overload to set a starting raw query
+        /// </summary>
+        /// <param name="rawQuery"></param>
+        public LookQuery(string rawQuery)
+        {
+           this.RawQuery = rawQuery;
+        }
     }
 }
