1687ac89f5e5e2a28da5cba78a9b51d609f49c8e
Hendy <hendyracher@hotmail.com>
2018-11-03 23:00:21 +0000

    service vars made private

--- src/Our.Umbraco.Look/Services/LookService.cs
+++ src/Our.Umbraco.Look/Services/LookService.cs
@@ -21,32 +21,32 @@ namespace Our.Umbraco.Look.Services
         /// <summary>
         /// Function to get the name for the IPublishedContent being indexed
         /// </summary>
-        internal Func<IPublishedContent, string> NameIndexer { get; set; } = x => LookService.DefaultNameIndexer(x);
+        private Func<IPublishedContent, string> NameIndexer { get; set; } = x => LookService.DefaultNameIndexer(x);
 
         /// <summary>
         /// Function to get the date for the IPublishedContent being indexed
         /// </summary>
-        internal Func<IPublishedContent, DateTime?> DateIndexer { get; set; } = x => LookService.DefaultDateIndexer(x);
+        private Func<IPublishedContent, DateTime?> DateIndexer { get; set; } = x => LookService.DefaultDateIndexer(x);
 
         /// <summary>
         /// Function to get text for the IPublishedContent being indexed
         /// </summary>
-        internal Func<IPublishedContent, string> TextIndexer { get; set; } = x => LookService.DefaultTextIndexer(x);
+        private Func<IPublishedContent, string> TextIndexer { get; set; } = x => LookService.DefaultTextIndexer(x);
 
         /// <summary>
         /// Function to get the tags for the IPublishedContent being indexed
         /// </summary>
-        internal Func<IPublishedContent, string[]> TagIndexer { get; set; } = x => LookService.DefaultTagIndexer(x);
+        private Func<IPublishedContent, string[]> TagIndexer { get; set; } = x => LookService.DefaultTagIndexer(x);
 
         /// <summary>
         /// Function to get a location for the IPublishedContent being indexed
         /// </summary>
-        internal Func<IPublishedContent, Location> LocationIndexer { get; set; } = x => null;
+        private Func<IPublishedContent, Location> LocationIndexer { get; set; } = x => null;
 
         /// <summary>
         /// Collection of cartesian tier plotters
         /// </summary>
-        internal List<CartesianTierPlotter> CartesianTierPlotters { get; } = new List<CartesianTierPlotter>();
+        private List<CartesianTierPlotter> CartesianTierPlotters { get; } = new List<CartesianTierPlotter>();
 
         /// <summary>
         /// Name of indexer to use (from configuration)
@@ -61,27 +61,27 @@ namespace Our.Umbraco.Look.Services
         /// <summary>
         /// Gets the Examine indexer
         /// </summary>
-        internal static BaseIndexProvider Indexer => ExamineManager.Instance.IndexProviderCollection[LookService.Instance.IndexerName];
+        private static BaseIndexProvider Indexer => ExamineManager.Instance.IndexProviderCollection[LookService.Instance.IndexerName];
 
         /// <summary>
         /// Get the Analyzer in use by Examine
         /// </summary>
-        internal static Analyzer Analyzer => ((LuceneIndexer)LookService.Indexer).IndexingAnalyzer;
+        private static Analyzer Analyzer => ((LuceneIndexer)LookService.Indexer).IndexingAnalyzer;
 
         /// <summary>
         /// Gets the Examine searcher
         /// </summary>
-        internal static BaseSearchProvider Searcher => ExamineManager.Instance.SearchProviderCollection[LookService.Instance.SearcherName];
+        private static BaseSearchProvider Searcher => ExamineManager.Instance.SearchProviderCollection[LookService.Instance.SearcherName];
 
         /// <summary>
         /// Max distance in miles for distance searches & location indexing
         /// </summary>
-        internal static double MaxDistance => 10000; // 12450 = half circumfrence of earth TODO: make configuration
+        private static double MaxDistance => 10000; // 12450 = half circumfrence of earth TODO: make configuration
 
         /// <summary>
         /// max numnber of results to request for a lucene query
         /// </summary>
-        internal static int MaxLuceneResults => 5000; // TODO: make configurable (maybe part of the SearchQuery obj)
+        private static int MaxLuceneResults => 5000; // TODO: make configurable (maybe part of the SearchQuery obj)
 
         /// <summary>
         /// Access the singleton instance of this search service
