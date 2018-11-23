7ec272262b462a9e9142ae123a2302d13483e4e4
Hendy <hendyracher@hotmail.com>
2018-11-23 21:01:35 +0000

    NameQuery validation to prevent excess wildcards

--- src/Our.Umbraco.Look/Models/NameQuery.cs
+++ src/Our.Umbraco.Look/Models/NameQuery.cs
@@ -1,28 +1,95 @@
-﻿namespace Our.Umbraco.Look.Models
+﻿using System;
+
+namespace Our.Umbraco.Look.Models
 {
     /// <summary>
     /// Search query criteria for use with the custom name field
     /// </summary>
     public class NameQuery
     {
-        /// <summary>
-        /// (Optional) set the string which the name must begin with
-        /// </summary>
-        public string StartsWith { get; set; }
+        private string _startsWith;
+
+        private string _endsWith;
+
+        private string _contains;
 
         /// <summary>
-        /// (Optional) set the string which the name must end with
+        /// (Optional) set a string which the name must begin with
         /// </summary>
-        public string EndsWith { get; set; }
+        public string StartsWith
+        {
+            get
+            {
+                return this._startsWith;
+            }
+
+            set
+            {
+                if (!this.ContainsWildcards(value))
+                {
+                    this._startsWith = value;
+                }
+                else
+                {
+                    throw new Exception($"StartsWith value must not contain any wildcard characters '*', '?'");
+                }
+            }
+        }
 
         /// <summary>
-        /// (Optional) set the string which must be present in the name
+        /// (Optional) set a string which the name must end with
         /// </summary>
-        public string Contains { get; set; }
+        public string EndsWith
+        {
+            get
+            {
+                return this._endsWith;
+            }
+
+            set
+            {
+                if (!this.ContainsWildcards(value))
+                {
+                    this._endsWith = value;
+                }
+                else
+                {
+                    throw new Exception($"EndsWith value must not contain any wildcard characters '*', '?'");
+                }
+            }
+        }
+
+        /// <summary>
+        /// (Optional) set a string which must be present in the name
+        /// </summary>
+        public string Contains
+        {
+            get
+            {
+                return this._contains;
+            }
+
+            set
+            {
+                if (!this.ContainsWildcards(value))
+                {
+                    this._contains = value;
+                }
+                else
+                {
+                    throw new Exception($"Contains value must not contain any wildcard characters '*', '?'");
+                }
+            }
+        }
 
         ///// <summary>
         ///// 
         ///// </summary>
         //public bool CaseSensitive { get; set; } = true;
+
+        private bool ContainsWildcards(string value)
+        {
+            return !value.Contains("*") && !value.Contains("?");
+        }
     }
 }
