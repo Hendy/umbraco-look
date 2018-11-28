ef87630ef872b675da704c7d35fa93f26ce89311
Hendy <hendyracher@hotmail.com>
2018-11-28 20:51:07 +0000

    empty query tests

---
+++ src/Our.Umbraco.Look.Tests/DemoSiteTests/LookQueryTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Our.Umbraco.Look.Models;
+using Our.Umbraco.Look.Services;
+
+namespace Our.Umbraco.Look.Tests.DemoSiteTests
+{
+    [TestClass]
+    public class LookQueryTests : BaseDemoSiteTests
+    {
+        [TestMethod]
+        public void Empty_Query()
+        {
+            var lookQuery = new LookQuery(this._searchingContext);
+
+            var lookResult = LookService.Query(lookQuery);
+
+            Assert.IsNotNull(lookResult);
+            Assert.IsFalse(lookResult.Success);
+            Assert.IsTrue(lookResult.Total == 0);
+        }
+
+    }
+}
