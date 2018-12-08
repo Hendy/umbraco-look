b98fb7f054487e7fc80f837c206c092c8578bd0a
Hendy Racher <git@hendyracher.co.uk>
2018-12-08 11:00:08 +0000

    unit test for NodeQuery model (pre non-breaking removal of constructor overloads)

---
+++ src/Our.Umbraco.Look.Tests/ModelTests/NodeQueryConstructorTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Our.Umbraco.Look.Models;
+using System.Linq;
+using Umbraco.Core.Models;
+
+namespace Our.Umbraco.Look.Tests.ModelTests
+{
+    [TestClass]
+    public class NodeQueryConstructorTests
+    {
+        [TestMethod]
+        public void Single_String_To_Array()
+        {
+            var nodeQuery1 = new NodeQuery("alias");
+            var nodeQuery2 = new NodeQuery() { Aliases = new string[] { "alias" }  };
+
+            Assert.AreEqual(nodeQuery1, nodeQuery2);
+        }
+
+        [TestMethod]
+        public void Node_Type_And_Single_String()
+        {
+            var nodeQuery1 = new NodeQuery(PublishedItemType.Content, "alias");
+            var nodeQuery2 = new NodeQuery()
+            {
+                Types = new PublishedItemType[] { PublishedItemType.Content },
+                Aliases = new string[] { "alias" }
+            };
+
+            Assert.AreEqual(nodeQuery1, nodeQuery2);
+        }
+    }
+}
