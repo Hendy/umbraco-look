ef0bd18bf219eb3d48664101bdb093926700c8f7
Hendy Racher <git@hendyracher.co.uk>
2018-12-09 13:15:37 +0000

    added commented out helper method

--- src/Our.Umbraco.Look/Models/Facet.cs
+++ src/Our.Umbraco.Look/Models/Facet.cs
@@ -13,5 +13,14 @@
         /// The total number of results expected should this facet be applied to the curent query
         /// </summary>
         public int Count { get; internal set; }
+
+        ///// <summary>
+        ///// helper to see if the tags collection for this facet is a single item, matching that supplied
+        ///// </summary>
+        ///// <param name="lookTag"></param>
+        //internal bool Equals(LookTag lookTag)
+        //{
+        //    return lookTag != null && this.Tags.Length == 1 && this.Tags[0].Equals(lookTag);
+        //}
     }
 }
