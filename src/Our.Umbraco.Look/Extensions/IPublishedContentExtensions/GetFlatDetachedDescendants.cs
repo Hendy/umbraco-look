394c2b6dbad28cbfcc2c20754cb86d78fe02f17e
Hendy <hendyracher@hotmail.com>
2018-12-17 17:05:56 +0000

    extension method to get all descandant detached IPublishedContent

---
+++ src/Our.Umbraco.Look/Extensions/IPublishedContentExtensions/GetFlatDetachedDescendants.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Umbraco.Core.Models;
+
+namespace Our.Umbraco.Look.Extensions
+{
+    internal static partial class IPublishedContentExtensions
+    {
+        /// <summary>
+        /// TODO: yield return
+        /// </summary>
+        /// <param name="publishedContent"></param>
+        /// <returns></returns>
+        internal static IEnumerable<IPublishedContent> GetFlatDetachedDescendants(this IPublishedContent publishedContent)
+        {
+            var detachedPublishedContent = new List<IPublishedContent>();
+
+            var properties = publishedContent
+                                .Properties
+                                .Where(y => y.Value is IEnumerable<IPublishedContent>)
+                                .Select(y => y.Value as IEnumerable<IPublishedContent>);
+
+            foreach (var property in properties)
+            {
+                detachedPublishedContent.AddRange(property);
+
+                // recurse
+                foreach (var propertyItem in property)
+                {
+                    detachedPublishedContent.AddRange(IPublishedContentExtensions.GetFlatDetachedDescendants(propertyItem));
+                }
+            }
+
+            return detachedPublishedContent.ToArray();
+        }
+    }
+}
