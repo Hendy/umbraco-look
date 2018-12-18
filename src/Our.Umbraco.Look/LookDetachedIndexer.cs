dcd5e27d56e69191a31a9fbdd616537e6bfb4ac7
Hendy <hendyracher@hotmail.com>
2018-12-18 22:09:37 +0000

    stubbing out overrides to see how back-office interacts with indexer & searcher

--- src/Our.Umbraco.Look/LookDetachedIndexer.cs
+++ src/Our.Umbraco.Look/LookDetachedIndexer.cs
@@ -1,18 +1,149 @@
-﻿using Examine.LuceneEngine.Providers;
+﻿using Examine;
+using Examine.LuceneEngine;
+using Examine.LuceneEngine.Config;
+using Examine.LuceneEngine.Providers;
+using Lucene.Net.Index;
+using Lucene.Net.Store;
 using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Xml.Linq;
 
 namespace Our.Umbraco.Look
 {
     public class LookDetachedIndexer : LuceneIndexer
     {
+        public override void Initialize(string name, NameValueCollection config)
+        {
+            base.Initialize(name, config);
+        }
+
+
+
         protected override void PerformIndexAll(string type)
         {
         }
 
+        /// <summary>
+        /// triggered from a back office rebuild 
+        /// </summary>
         protected override void PerformIndexRebuild()
         {
-            // triggered from a back office rebuild 
-            this.OnIndexOperationComplete(new EventArgs()); // causes the backoffice rebuild to end
+
+            this.OnIndexOperationComplete(new EventArgs()); // causes the backoffice rebuild to end (UI thread watches a cache value)
+        }
+
+        protected override void AddDocument(Dictionary<string, string> fields, IndexWriter writer, int nodeId, string type)
+        {
+            base.AddDocument(fields, writer, nodeId, type);
+        }
+        protected override void AddSingleNodeToIndex(XElement node, string type)
+        {
+            base.AddSingleNodeToIndex(node, type);
+        }
+        protected override IndexWriter CreateIndexWriter()
+        {
+            return base.CreateIndexWriter();
+        }
+        public override void DeleteFromIndex(string nodeId)
+        {
+            base.DeleteFromIndex(nodeId);
+        }
+        protected override Dictionary<string, string> GetDataToIndex(XElement node, string type)
+        {
+            return base.GetDataToIndex(node, type);
+        }
+        protected override IIndexCriteria GetIndexerData(IndexSet indexSet)
+        {
+            return base.GetIndexerData(indexSet);
+        }
+        public override Directory GetLuceneDirectory()
+        {
+            return base.GetLuceneDirectory();
+        }
+        protected override FieldIndexTypes GetPolicy(string fieldName)
+        {
+            return base.GetPolicy(fieldName);
+        }
+        protected override Dictionary<string, string> GetSpecialFieldsToIndex(Dictionary<string, string> allValuesForIndexing)
+        {
+            return base.GetSpecialFieldsToIndex(allValuesForIndexing);
+        }
+        public override void IndexAll(string type)
+        {
+            base.IndexAll(type);
+        }
+        public override bool IndexExists()
+        {
+            return base.IndexExists();
+        }
+        protected override void OnDocumentWriting(DocumentWritingEventArgs docArgs)
+        {
+            base.OnDocumentWriting(docArgs);
+        }
+        protected override void OnDuplicateFieldWarning(int nodeId, string indexSetName, string fieldName)
+        {
+            base.OnDuplicateFieldWarning(nodeId, indexSetName, fieldName);
+        }
+        protected override void OnGatheringFieldData(IndexingFieldDataEventArgs e)
+        {
+            base.OnGatheringFieldData(e);
+        }
+        protected override void OnGatheringNodeData(IndexingNodeDataEventArgs e)
+        {
+            base.OnGatheringNodeData(e);
+        }
+        protected override void OnIgnoringNode(IndexingNodeDataEventArgs e)
+        {
+            base.OnIgnoringNode(e);
+        }
+        protected override void OnIndexDeleted(DeleteIndexEventArgs e)
+        {
+            base.OnIndexDeleted(e);
+        }
+        protected override void OnIndexingError(IndexingErrorEventArgs e)
+        {
+            base.OnIndexingError(e);
+        }
+        protected override void OnIndexOperationComplete(EventArgs e)
+        {
+            base.OnIndexOperationComplete(e);
+        }
+        protected override void OnIndexOptimized(EventArgs e)
+        {
+            base.OnIndexOptimized(e);
+        }
+        protected override void OnIndexOptimizing(EventArgs e)
+        {
+            base.OnIndexOptimizing(e);
+        }
+        protected override void OnNodeIndexed(IndexedNodeEventArgs e)
+        {
+            base.OnNodeIndexed(e);
+        }
+        protected override void OnNodeIndexing(IndexingNodeEventArgs e)
+        {
+            base.OnNodeIndexing(e);
+        }
+        protected override void OnNodesIndexed(IndexedNodesEventArgs e)
+        {
+            base.OnNodesIndexed(e);
+        }
+        protected override void OnNodesIndexing(IndexingNodesEventArgs e)
+        {
+            base.OnNodesIndexing(e);
+        }
+        public override void RebuildIndex()
+        {
+            base.RebuildIndex();
+        }
+        public override void ReIndexNode(XElement node, string type)
+        {
+            base.ReIndexNode(node, type);
+        }
+        protected override bool ValidateDocument(XElement node)
+        {
+            return base.ValidateDocument(node);
         }
     }
 }
--- src/Our.Umbraco.Look/LookDetachedSearcher.cs
+++ src/Our.Umbraco.Look/LookDetachedSearcher.cs
@@ -1,13 +1,87 @@
-﻿using Examine.LuceneEngine.Providers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Examine;
+using Examine.LuceneEngine.Providers;
+using Examine.SearchCriteria;
+using Lucene.Net.Index;
+using Lucene.Net.Search;
+using Lucene.Net.Store;
+using System.Collections.Specialized;
 
 namespace Our.Umbraco.Look
 {
     public class LookDetachedSearcher : LuceneSearcher
     {
+        //// did this get called ?
+        //public override void Initialize(string name, NameValueCollection config)
+        //{
+        //    base.Initialize(name, config);
+        //}
+
+        //// not required to override, as it returns all lucene fields (TODO: test with tag generated fields)
+        //protected override string[] GetSearchFields()
+        //{
+        //    var debug = base.GetSearchFields();
+        //    return debug;
+        //}
+
+        //public override ISearchCriteria CreateSearchCriteria()
+        //{
+        //    var debug = base.CreateSearchCriteria();
+        //    return debug;
+        //}
+        //public override ISearchCriteria CreateSearchCriteria(BooleanOperation defaultOperation)
+        //{
+        //    var debug = base.CreateSearchCriteria(defaultOperation);
+        //    return debug;
+        //}
+        //public override ISearchCriteria CreateSearchCriteria(string type)
+        //{
+        //    var debug = base.CreateSearchCriteria(type);
+        //    return debug;
+        //}
+        //public override ISearchCriteria CreateSearchCriteria(string type, BooleanOperation defaultOperation)
+        //{
+        //    var debug = base.CreateSearchCriteria(type, defaultOperation);
+        //    return debug;
+        //}
+
+        //public override Searcher GetSearcher()
+        //{
+        //    var debug = base.GetSearcher();
+        //    return debug;
+        //}
+        //protected override Directory GetLuceneDirectory()
+        //{
+        //    var debug = base.GetLuceneDirectory();
+        //    return debug;
+        //}
+        //protected override IndexReader OpenNewReader()
+        //{
+        //    var debug = base.OpenNewReader();
+        //    return debug;
+        //}
+
+
+        public override ISearchResults Search(ISearchCriteria searchParams)
+        {
+            var debug = new LookQuery(this.Name) { ExamineQuery = searchParams }.Run();
+            return debug;
+        }
+        public override ISearchResults Search(ISearchCriteria searchParams, int maxResults)
+        {
+            var debug = new LookQuery(this.Name) { ExamineQuery = searchParams }.Run();
+            return debug;
+        }
+        public override ISearchResults Search(string searchText, bool useWildcards)
+        {
+            var debug = new LookQuery(this.Name) { TextQuery = new TextQuery(searchText) }.Run();
+            return debug;
+        }
+        public override ISearchResults Search(string searchText, bool useWildcards, string indexType)
+        {
+            var debug = new LookQuery(this.Name) { TextQuery = new TextQuery(searchText) }.Run();
+            return debug;
+        }
+
+
     }
 }
