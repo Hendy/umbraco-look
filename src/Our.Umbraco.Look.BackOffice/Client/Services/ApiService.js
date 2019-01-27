0afc358d1a102ff791fc17df4341422cff6b332a
Hendy <hendyracher@hotmail.com>
2019-01-27 19:05:22 +0000

    added ApiService.js

---
+++ src/Our.Umbraco.Look.BackOffice/Client/Services/ApiService.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('umbraco')
+        .factory('Look.BackOffice.ApiService', ApiService);
+
+    ApiService.$inject = ['$rootScope', '$http', '$q'];
+
+    function ApiService($rootScope, $http, $q) {
+
+        return {
+            getSearcherDetails: getSearcherDetails
+        };
+     
+        function getSearcherDetails(searcherName) {
+
+            return $http({
+                method: 'GET',
+                url: 'BackOffice/Look/Api/GetSearcherDetails',
+                params: {
+                    'searcherName': searcherName
+                }
+            });
+        }
+
+    }
+
+})();
