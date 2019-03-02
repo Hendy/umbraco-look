e6710224f72130220f55acd5bf2b8873628330f5
Hendy <hendyracher@hotmail.com>
2019-03-02 18:37:12 +0000

    added client js treeService

---
+++ src/Our.Umbraco.Look.BackOffice/Client/Services/TreeService.js
@@ -0,0 +1,28 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('umbraco')
+        .factory('Look.BackOffice.TreeService', TreeService);
+
+    TreeService.$inject = ['navigationService', '$timeout'];
+
+    function TreeService(navigationService, $timeout) {
+
+        return { update: update };
+
+        // register a callback to be triggered on change
+        function update(path) {
+            console.log('tree service !');
+            $timeout(function () { // timeout required to ensure navigationService ready
+                navigationService.syncTree({
+                    tree: 'lookTree',
+                    path: path,
+                    forceReload: true
+                });
+            }, 500);
+
+        }
+    }
+
+})();
