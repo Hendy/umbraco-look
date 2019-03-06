62baf12eb5c69a779db4e508d084e4693d771e52
Hendy Racher <git@hendyracher.co.uk>
2019-03-06 14:59:42 +0000

    updated versioning to Look 0.35.0 and BackOffice 0.1.2

--- src/Our.Umbraco.Look.BackOffice/Properties/AssemblyInfo.cs
+++ src/Our.Umbraco.Look.BackOffice/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.2.0")]
+[assembly: AssemblyFileVersion("0.1.2.0")]
 
 //[assembly: EmbeddedResourceProtected("Our.Umbraco.Look.BackOffice.Resources.Controllers.SearcherController.js", "/App_Plugins/Look/SearcherController.js")]
 //[assembly: EmbeddedResourceProtected("Our.Umbraco.Look.BackOffice.Resources.Controllers.TagController.js", "/App_Plugins/Look/TagController.js")]
