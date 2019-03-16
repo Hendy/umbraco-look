595bac53b673e331f6bc41955548080e47681779
Hendy Racher <git@hendyracher.co.uk>
2019-03-16 09:05:02 +0000

    only set the http context for the thread if not already done so

--- src/Our.Umbraco.Look/Services/LookService_SetExamineIndexers.cs
+++ src/Our.Umbraco.Look/Services/LookService_SetExamineIndexers.cs
@@ -92,6 +92,12 @@ namespace Our.Umbraco.Look.Services
         }
 
         /// <summary>
+        /// Flag used to determine whether the http context has been ensured for the current thread
+        /// </summary>
+        [ThreadStatic]
+        private static bool ContextEnsured = false;
+
+        /// <summary>
         /// 
         /// </summary>
         /// <param name="sender"></param>
@@ -122,16 +128,21 @@ namespace Our.Umbraco.Look.Services
 
             if (publishedContent != null)
             {
-                var dummyHttpContext = new HttpContextWrapper(new HttpContext(new SimpleWorkerRequest("", "", new StringWriter())));
+                if (!LookService.ContextEnsured)
+                {
+                    var httpContext = new HttpContextWrapper(new HttpContext(new SimpleWorkerRequest("", "", new StringWriter())));
 
-                UmbracoContext.EnsureContext(
-                                    dummyHttpContext,
-                                    ApplicationContext.Current,
-                                    new WebSecurity(dummyHttpContext, ApplicationContext.Current),
-                                    UmbracoConfig.For.UmbracoSettings(),
-                                    UrlProviderResolver.Current.Providers,
-                                    true,
-                                    false);
+                    UmbracoContext.EnsureContext(
+                                        httpContext,
+                                        ApplicationContext.Current,
+                                        new WebSecurity(httpContext, ApplicationContext.Current),
+                                        UmbracoConfig.For.UmbracoSettings(),
+                                        UrlProviderResolver.Current.Providers,
+                                        true,
+                                        false);
+
+                    LookService.ContextEnsured = true;
+                }
 
                 var indexingContext = new IndexingContext(
                                             hostNode: null,
