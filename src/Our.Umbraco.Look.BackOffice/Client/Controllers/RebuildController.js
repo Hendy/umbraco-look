595a43e00c3212f1296b459bb0439c8c2bad5188
Hendy <hendyracher@hotmail.com>
2019-03-17 21:27:29 +0000

    #78 rebuild index ui now has 3 states, start/error, doing & complete

--- src/Our.Umbraco.Look.BackOffice/Client/Controllers/RebuildController.js
+++ src/Our.Umbraco.Look.BackOffice/Client/Controllers/RebuildController.js
@@ -5,9 +5,17 @@
         .module('umbraco')
         .controller('Look.BackOffice.RebuildController', RebuildController);
 
-    RebuildController.$inject = ['$scope', '$routeParams', 'Look.BackOffice.ApiService', 'navigationService'];
+    RebuildController.$inject = ['$scope', 'Look.BackOffice.ApiService'];
 
-    function RebuildController($scope, $routeParams, apiService, navigationService) {
+    function RebuildController($scope, apiService) {
+
+        $scope.viewData = {
+            ready: false,
+            indexing: false,
+            indexingComplete: false
+        };
+
+        //$scope.viewData.indexing = false; // TODO: get this from a shared service
 
         // init function used to get the tree node id from the view ! (as can't find a suitable resource to inject so as to get at this value)
         // the current node represents the tree node associated with the current menu option (not necessarilty the currently selected tree node)
@@ -23,20 +31,21 @@
         };
 
         $scope.hide = function () {
-            navigationService.hideNavigation(); // TODO: fix this so only the menu closes - no redirecting
+            // TODO: close the menu - no redirecting
         };
 
         $scope.rebuild = function () {
 
             // TODO: set flag on service to indicate this index is being rebuilt (tree rendering will also use this)
+            $scope.viewData.indexing = true;
 
             apiService.rebuildIndex($scope.viewData.indexerName)
                 .then(function (response) {
 
+                    // TODO: tell service indexing is complete, as that will be watched
+                    $scope.viewData.indexing = false;
+                    $scope.viewData.indexingComplete = true;
                 });
-            
-
-            // TODO: setup a polling func to reset flag, once any search results are found
         };
     }
 
