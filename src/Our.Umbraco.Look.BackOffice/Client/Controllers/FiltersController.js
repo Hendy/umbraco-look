5801527474b6987977cd168e62359c72c6af0db7
Hendy Racher <git@hendyracher.co.uk>
2019-03-27 12:12:55 +0000

    empty filters controller & view

---
+++ src/Our.Umbraco.Look.BackOffice/Client/Controllers/FiltersController.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('umbraco')
+        .controller('Look.BackOffice.FiltersController', FiltersController);
+
+    FiltersController.$inject = ['$scope'];
+
+    function FiltersController($scope) {
+
+        $scope.debug = "filters";
+
+    }
+
+})();
