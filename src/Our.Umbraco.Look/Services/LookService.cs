b7d0517ab5eb096a06c58a5b9a7c0b0231d33a36
Hendy <hendyracher@hotmail.com>
2019-04-04 21:28:31 +0100

    indexIf functional

--- src/Our.Umbraco.Look/Services/LookService.cs
+++ src/Our.Umbraco.Look/Services/LookService.cs
@@ -38,9 +38,9 @@ namespace Our.Umbraco.Look.Services
         private Dictionary<string, Directory> _indexSetDirectories;
 
         /// <summary>
-        /// Function called before indexing, to know if it should continue the indexing process
+        /// Function called before indexing, to know if it should continue the indexing process (defaults to true)
         /// </summary>
-        private Func<IndexingContext, bool> _indexIf;
+        private Func<IndexingContext, bool> _indexIf = IndexingContext => true;
 
         /// <summary>
         /// Function to get the name for the IPublishedContent being indexed
