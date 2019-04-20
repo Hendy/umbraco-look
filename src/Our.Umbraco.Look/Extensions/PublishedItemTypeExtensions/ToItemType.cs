1290659e9537323ae17eadb6f0022755f0c792c0
Hendy <hendyracher@hotmail.com>
2019-04-20 21:05:15 +0100

    ToItemType extension method on PublishedItemType enum

---
+++ src/Our.Umbraco.Look/Extensions/PublishedItemTypeExtensions/ToItemType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Umbraco.Core.Models;
+
+namespace Our.Umbraco.Look.Extensions
+{
+    internal static partial class PublishedItemTypeExtensions
+    {
+        /// <summary>
+        /// Helper to convert from an Umbraco PublishedItemType enum value to a local enum value (which adds detached options for each)
+        /// </summary>
+        /// <param name="value">The PublishedItemType value to convert from</param>
+        /// <returns>The corresponding ItemType value</returns>
+        internal static ItemType ToItemType(this PublishedItemType publishedItemType)
+        {
+            switch(publishedItemType)
+            {
+                case PublishedItemType.Content: return ItemType.Content;
+                case PublishedItemType.Media: return ItemType.Media;
+                case PublishedItemType.Member: return ItemType.Member;
+            }
+
+            throw new Exception("Unown PublishedItemType");
+        }
+    }
+}
