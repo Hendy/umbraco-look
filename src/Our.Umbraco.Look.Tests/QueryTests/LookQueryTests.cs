5543731e8af167e873a362cadadde9687cb4ba73
Hendy <hendyracher@hotmail.com>
2020-01-05 09:47:16 +0000

    unit tests for MaxResults (failing)

--- src/Our.Umbraco.Look.Tests/QueryTests/LookQueryTests.cs
+++ src/Our.Umbraco.Look.Tests/QueryTests/LookQueryTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 using Umbraco.Core.Models;
 
 namespace Our.Umbraco.Look.Tests.QueryTests
@@ -6,6 +7,19 @@ namespace Our.Umbraco.Look.Tests.QueryTests
     [TestClass]
     public class LookQueryTests
     {
+        /// <summary>
+        /// Ensure there are 100 things in the index
+        /// </summary>
+        /// <param name="testContext"></param>
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            TestHelper.IndexThings(
+                Enumerable
+                .Range(1, 100)
+                .Select(x => new Thing()));
+        }
+
         [TestMethod]
         public void Empty_Query()
         {
@@ -31,5 +45,40 @@ namespace Our.Umbraco.Look.Tests.QueryTests
             Assert.IsNotNull(lookResult);
             Assert.IsTrue(lookResult.Success);
         }
+
+        [TestMethod]
+        public void Max_Results_Value_Valid()
+        {
+            var lookQuery = new LookQuery(TestHelper.GetSearchingContext());
+
+            lookQuery.NameQuery = new NameQuery(); // set a query clause so it's acutally executed
+            
+            lookQuery.MaxResults = 5;
+
+            var lookResult = lookQuery.Search();
+
+            Assert.IsNotNull(lookResult);
+            Assert.IsTrue(lookResult.Success);
+            //Assert.AreEqual(5, lookResult.TotalItemCount);
+            Assert.AreEqual(5, lookResult.Matches.Count());
+        }
+
+        [TestMethod]
+        public void Max_Results_Value_Invalid_Negative()
+        {
+            var lookQuery = new LookQuery(TestHelper.GetSearchingContext());
+
+            lookQuery.NameQuery = new NameQuery(); // set a query clause so it's acutally executed
+
+            lookQuery.MaxResults = -1;
+
+            var lookResult = lookQuery.Search();
+
+            Assert.IsNotNull(lookResult);
+            Assert.IsTrue(lookResult.Success);
+            //Assert.AreEqual(100, lookResult.TotalItemCount);
+            Assert.AreEqual(100, lookResult.Matches.Count());
+        }
+
     }
 }
